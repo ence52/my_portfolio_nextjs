ce715b6a396df9e17e247616d8141261cc261f0a
Erkan Ince <ince52erkan@gmail.com>
2025-02-05 19:50:54 +0300

    meta info added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Erkan INCE | Portfolio" />
+        <meta
+          property="og:description"
+          content="Check out my projects and technologies I use!"
+        />
+        <meta
+          property="og:image"
+          content="https://erkanince.vercel.app/images/hero/hero.png"
+        />
+        <meta property="og:url" content="https://erkanince.vercel.app/" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
