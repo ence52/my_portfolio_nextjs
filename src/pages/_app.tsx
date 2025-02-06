f9b7a36901cb3dec20962426522b44f4b98232ad
Erkan Ince <ince52erkan@gmail.com>
2025-02-06 17:07:55 +0300

    twitter card meta added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,17 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <meta property="og:url" content="https://erkanince.vercel.app/" />
         <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Erkan INCE | Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Check out my projects and technologies I use!"
+        />
+        <meta
+          name="twitter:image"
+          content="https://erkanince.vercel.app/images/hero/hero.png"
+        />
       </Head>
       <Component {...pageProps} />
       <Analytics />
