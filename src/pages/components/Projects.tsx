92a9156ef26130206ee1f0b8d01a768b72ac5e9c
Erkan Ince <ince52erkan@gmail.com>
2025-06-14 01:50:55 +0300

    apple app added

--- src/pages/components/Projects.tsx
+++ src/pages/components/Projects.tsx
@@ -59,6 +59,11 @@ const lambAppPhotos = [
   "/images/projects/lamb_app/2.png",
   "/images/projects/lamb_app/3.png",
 ];
+const appleAppPhotos = [
+  "/images/projects/apple_store_ui/1.png",
+  "/images/projects/apple_store_ui/2.png",
+  "/images/projects/apple_store_ui/3.png",
+];
 const chatAppPhotos = [
   "/images/projects/chat_app/1.png",
   "/images/projects/chat_app/2.png",
@@ -139,6 +144,16 @@ const projectList: ProjectInfo[] = [
   },
   {
     id: 4,
+    name: "React Native Apple Store App UI",
+    description:
+      "A responsive implementation of a publicly sourced design, developed using React Native and Tailwind CSS.",
+    images: appleAppPhotos,
+    mobile: true,
+    techs: ["React Native", "Html", "CSS", "TypeScript", "VS Code"],
+    githubLink: "https://github.com/ence52/react_native_apple_store_ui",
+  },
+  {
+    id: 5,
     name: "Lamb Mobile App UI",
     description:
       "It is a mobile UI project for custom-designed lamp sales, based on a design found online and implemented with Flutter.",
@@ -149,7 +164,7 @@ const projectList: ProjectInfo[] = [
   },
 
   {
-    id: 5,
+    id: 6,
     name: "E-Commerce API",
     description:
       "This is the backend API for an E-Commerce platform, built with FastAPI and Python. It handles product listings, user authentication, order management, and payment processing. The app is powered by MySQL for database management and Docker for containerization, ensuring scalability and ease of deployment.",
@@ -159,7 +174,7 @@ const projectList: ProjectInfo[] = [
     githubLink: "https://github.com/ence52/e-commerce_app_backend",
   },
   {
-    id: 6,
+    id: 7,
     name: "Bakery App",
     description:
       "The Bakery App allows users to explore a variety of bakery products, view their details, and place orders directly from the app. Built with Flutter, this mobile app provides an engaging user experience with smooth navigation and real-time updates for product availability.",
@@ -169,7 +184,7 @@ const projectList: ProjectInfo[] = [
     githubLink: "https://github.com/ence52/bakery_app_ui",
   },
   {
-    id: 7,
+    id: 8,
     name: "Car Selling App",
     description:
       "This app enables users to browse and list cars for sale. With its intuitive design, users can upload their car listings with ease, view available cars, and filter results based on their preferences. The app was built using Flutter to provide a native-like experience on both Android and iOS platforms.",
@@ -179,7 +194,7 @@ const projectList: ProjectInfo[] = [
     githubLink: "https://github.com/ence52/car_selling_app_ui",
   },
   {
-    id: 8,
+    id: 9,
     name: "ListRace Website",
     description:
       "The ListRace website is a dynamic platform built with React. It allows users to create and view lists, track race results, and interact with various features through a clean and user-friendly interface. The website is designed for seamless navigation, offering a responsive experience on both desktop and mobile devices.",
@@ -189,7 +204,7 @@ const projectList: ProjectInfo[] = [
     githubLink: "https://github.com/ence52/listrace_react_ui",
   },
   {
-    id: 9,
+    id: 10,
     name: ".Net API",
     description:
       "This is a RESTful API built using .NET Core and C#, providing a solid backend for handling data storage and real-time processing. It integrates MongoDB for efficient data storage and Redis for caching to improve performance. Docker is used for containerization, ensuring smooth deployment across environments.",
